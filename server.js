46da9185ac8eba41d4e432c6cf2380d84d261ce5
a270202 <akshitcryptoo@gmail.com>
2025-01-18 15:40:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/auth');
+const connectDB = require('./db');
+const authRoutes = require('./auth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
