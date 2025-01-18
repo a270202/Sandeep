9e292cd000f1ceada77346ff9be213062766edca
a270202 <akshitcryptoo@gmail.com>
2025-01-18 15:44:17 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import Dashboard from './Dashboard';
+import SubDashboard from './SubDashboard';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/text1" element={<SubDashboard title="Text1 Dashboard" />} />
+        <Route path="/dashboard/text2" element={<SubDashboard title="Text2 Dashboard" />} />
+        <Route path="/dashboard/text3" element={<SubDashboard title="Text3 Dashboard" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
