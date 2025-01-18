e10876b9f159fa35246a54e867507d73a6e5443d
a270202 <akshitcryptoo@gmail.com>
2025-01-18 16:28:31 +0530

    Update SubDashboard.js

--- SubDashboard.js
+++ SubDashboard.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SubDashboard = ({ title }) => {
+  const links = [
+    { text: 'Open File 1', file: 'file1.html' },
+    { text: 'Open File 2', file: 'file2.html' },
+    { text: 'Open File 3', file: 'file3.html' },
+  ];
+
   return (
     <div className="subdashboard-container">
       <h1>{title}</h1>
       <div className="cards">
-        <div className="card">Card 1</div>
-        <div className="card">Card 2</div>
-        <div className="card">Card 3</div>
+        {links.map((link, index) => (
+          <Link key={index} to={`/dashboard/${title.toLowerCase()}/${link.file}`}>
+            <button>{link.text}</button>
+          </Link>
+        ))}
       </div>
     </div>
   );
