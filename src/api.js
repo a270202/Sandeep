6aa3fab834fa7e49d1a56beacb9dcfd20694323a
a270202 <akshitcryptoo@gmail.com>
2025-01-19 18:17:37 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'https://a270202.github.io/Sandeep/api',
-});
-
-export const signup = (data) => API.post('/auth/signup', data);
-export const login = (data) => API.post('/auth/login', data);
